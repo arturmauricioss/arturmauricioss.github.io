0ddbc20351a302cf05d7de5f3284370a843bf16d
arturmauricioss <arturmauricioss@outlook.com>
2024-06-11 18:00:43 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Primeiro App React</h1>
+      <p>Bem-vindo ao meu aplicativo!</p>
+    </div>
+  );
+}
+
+export default App;
